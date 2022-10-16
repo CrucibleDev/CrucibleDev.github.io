0595944bdcc3dfd9827169c468490c021ba7584b
Crucible <Nicksch314@gmail.com>
2022-10-16 12:31:45 -0500

    Version 0.0.007

--- templates.js
+++ templates.js
@@ -15,7 +15,7 @@ var headTemplateHTML = `
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="CSS/crucibleStyle.css">
+<link rel="stylesheet" href="CSS/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 `;
 
